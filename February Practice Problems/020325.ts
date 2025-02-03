ff537539d20cb0bab586965681163e2a5ff07a2e
StefanosTheDev <stefanosdev26@gmail.com>
2025-02-03 10:31:32 -0800

    push

--- February Practice Problems/020325.ts
+++ February Practice Problems/020325.ts
@@ -37,3 +37,49 @@ const target = 9;
 
 const result = twoSum(nums, target);
 console.log(`Indices: ${result[0]}, ${result[1]}`); // Output: [0, 1]
+
+function isValid(s: string): boolean {
+  const key: string = '(){}[]';
+  for (let i = 0; i < s.length; i++) {
+    console.log(s[i]);
+    // Okay I need to find the type
+  }
+  return true;
+}
+isValid('(){');
+/**
+ * 
+ * 
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+ */
